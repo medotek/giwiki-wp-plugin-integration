6626997c0ae951ec193cc17478de34508e3c3283
Thomas Huynh <thomas.huynh@etu.univ-grenoble-alpes.fr>
2021-09-13 21:23:58 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,57 @@
-// Webpack uses this to work with directories
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require('path');
-const env = process.env.NODE_ENV;
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const dev = process.env.NODE_ENV === 'dev'
 
-// This is the main configuration object.
-// Here, you write different options and tell Webpack what to do
-module.exports = {
-
-    // Path to your entry point. From this file Webpack will begin its work
-    entry: './medo-index.js',
-
-    // Path and filename of your result bundle.
-    // Webpack will bundle all JavaScript into this file
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '',
-        filename: 'bundle.js'
+const styleLoaders = [
+    {
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+            hmr: process.env.NODE_ENV === 'development'
+        }
     },
+    'css-loader'
+]
 
+const config = {
+    entry: {
+        app: ['./assets/scss/app.scss', './assets/js/app.js']
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve('./assets/js/'),
+            '@css': path.resolve('./assets/css/'),
+            '@scss': path.resolve('./assets/scss/')
+        }
+    },
+    watch: dev,
+    mode: dev ? 'development' : 'production',
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.bundle.css'
+        })
+    ],
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                exclude: /(node_modules)/,
+                use: ['eslint-loader']
+            },
+            {
+                test: /\.css$/,
+                use: styleLoaders
             },
             {
                 test: /\.scss$/,
-                exclude: /node_modules/,
-                use: [
-                    { loader: MiniCssExtractPlugin.loader },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            importLoaders: 1
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                            config: {
-                                ctx: {
-                                    cssnano: {},
-                                    autoprefixer: {}
-                                }
-                            }
-                        }
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            sourceMap: true // il est indispensable d'activer les sourcemaps pour que postcss fonctionne correctement
-                        }
-                    },
-                    {
-                        loader: 'style-loader',
-                    }
-                ]
+                use: [...styleLoaders, 'sass-loader']
             },
         ]
     },
+    output: {
+        path: path.resolve(__dirname, 'public/assets'),
+        filename: '[name].bundle.js'
+    }
 
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        }),
-    ],
+}
 
-    mode: env || 'development',
-};
+module.exports = config
