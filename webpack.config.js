f2f4736c955e74942fbab3643d1b5bbf544bf377
Thomas Huynh <thomas.huynh@etu.univ-grenoble-alpes.fr>
2021-09-13 21:03:00 +0200

    Added other dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.sc|ass$/,
+                test: /\.scss$/,
                 use: [
                     { loader: MiniCssExtractPlugin.loader },
                     {
